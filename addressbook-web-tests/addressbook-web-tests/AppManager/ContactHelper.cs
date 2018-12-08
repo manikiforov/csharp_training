f622920df961f52d73f26807496bfeed439a0e39
Максим Никифоров <maxim.nikiforov.tomsk@gmail.com>
2018-12-08 11:21:07 +0000

    Lecture 5.3 - Completed

--- addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
@@ -19,7 +19,7 @@ namespace WebAddressbookTests
 
         {
         }
-
+               
         public ContactHelper Create(ContactData contact)
         {
             AddNewContact();
@@ -159,6 +159,56 @@ namespace WebAddressbookTests
         public int GetContactCount()
         {
             return driver.FindElements(By.XPath("//*[@id='maintable']/tbody/tr[@name='entry']")).Count;
+        }
+
+        internal ContactData GetContactInformationFromTable(int index)
+        {
+            manager.Navigator.GoToHomePage();
+
+            IList<IWebElement> cells = driver.FindElements(By.Name("entry"))[index].FindElements(By.TagName("td"));
+            string lastName = cells[1].Text;
+            string firstName = cells[2].Text;
+            string address = cells[3].Text;
+            string allemails = cells[4].Text;
+            string allPhones = cells[5].Text;
+
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                AllPhones = allPhones,
+                AllEmails = allemails
+            };
+
+        }
+
+        internal ContactData GetContactInformationFromEditForm(int index)
+        {
+            manager.Navigator.GoToHomePage();
+            InitContactModification(index);
+
+            string firstName = driver.FindElement(By.Name("firstname")).GetAttribute("value");
+            string lastName = driver.FindElement(By.Name("lastname")).GetAttribute("value");
+            string address = driver.FindElement(By.Name("address")).GetAttribute("value");
+
+            string homePhone = driver.FindElement(By.Name("home")).GetAttribute("value");
+            string mobilePhone = driver.FindElement(By.Name("mobile")).GetAttribute("value");
+            string workPhone = driver.FindElement(By.Name("work")).GetAttribute("value");
+
+            string email = driver.FindElement(By.Name("email")).GetAttribute("value");
+            string email2 = driver.FindElement(By.Name("email2")).GetAttribute("value");
+            string email3 = driver.FindElement(By.Name("email3")).GetAttribute("value");
+
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                HomePhone = homePhone,
+                WorkPhone = workPhone,
+                MobilePhone = mobilePhone,
+                Email = email,
+                Email2 = email2,
+                Email3 = email3
+            };
+
         }
 
     }
--- addressbook-web-tests/addressbook-web-tests/Model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/Model/ContactData.cs
@@ -8,7 +8,10 @@ namespace WebAddressbookTests
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
-        
+        private string allPhones;
+        private string allEmails;
+
+
         public ContactData(string firstname, string lastname)
         {
             Firstname = firstname;
@@ -61,7 +64,70 @@ namespace WebAddressbookTests
         public string Lastname { get; set; }
         
         public string Nickname { get; set; }
-        
+
         public string ContactId { get; set; }
+
+        public string Address { get; set; }
+
+        public string HomePhone { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string WorkPhone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Email2 { get; set; }
+
+        public string Email3 { get; set; }
+
+        public string AllPhones
+        {
+            get
+            {
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone)).Trim();
+                }
+            }
+
+            set
+            {
+                allPhones = value;
+            }
+        }
+
+        public string AllEmails
+        {
+            get
+            {
+                if (allEmails != null)
+                {
+                    return allEmails;
+                }
+                else
+                {
+                    return (CleanUp(Email) + CleanUp(Email2) + CleanUp(Email3)).Trim();
+                }
+            }
+
+            set
+            {
+                allEmails = value;
+            }
+        }
+
+        private string CleanUp(string item)
+        {
+            if (item == null || item == "")
+            {
+                return "";
+            }
+            return item.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+        }
     }
 }
