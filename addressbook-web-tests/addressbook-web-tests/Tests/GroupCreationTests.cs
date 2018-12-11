fe7b9f28446a1d257c5b3b7c08138dff84abd6ab
Максим Никифоров <maxim.nikiforov.tomsk@gmail.com>
2018-12-11 19:54:29 +0300

    Lecture 6.1 - Completed

--- addressbook-web-tests/addressbook-web-tests/Tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/GroupCreationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -16,6 +17,18 @@ namespace WebAddressbookTests
             for (int i=0; i<5; i++)
             {
                 groups.Add(new GroupData(GenerateRandomString(30)) { Header = GenerateRandomString(100), Footer = GenerateRandomString(100) });
+            }
+            return groups;
+        }
+
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+               string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0]) { Header = parts[1], Footer = parts[2] });
             }
             return groups;
         }
@@ -39,7 +52,25 @@ namespace WebAddressbookTests
             
         }
 
-        
+        [Test, TestCaseSource("GroupDataFromFile")]
+
+        public void GroupCreationTestFromFile(GroupData group)
+        {
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Create(group);
+
+            Assert.AreEqual(oldGroups.Count + 1, app.Groups.GetGroupCount());
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(group);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+
+        }
+
         [Test]
         public void BadNameGroupCreationTest()
         {
