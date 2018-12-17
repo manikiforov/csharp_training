1b5598f02c5b6076d71fc20ffd873fc33c14b013
Максим Никифоров <maxim.nikiforov.tomsk@gmail.com>
2018-12-17 18:52:23 +0300

    Lecture 7.1

--- addressbook-web-tests/addressbook-web-tests/Tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/GroupCreationTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
@@ -197,6 +198,21 @@ namespace WebAddressbookTests
             newGroups.Sort();
             Assert.AreEqual(oldGroups, newGroups);
 
+        }
+
+        /* CREATE GROUP FROM DB */
+        [Test]
+        public void TestDBConnectivety()
+        {
+            DateTime start = DateTime.Now;
+            List<GroupData> fromUI = app.Groups.GetGroupList();
+            DateTime end = DateTime.Now;
+            System.Console.Out.WriteLine(end.Subtract(start));
+
+            start = DateTime.Now;
+            List<GroupData> fromDb = GroupData.GetAll();
+            end = DateTime.Now;
+            System.Console.Out.WriteLine(end.Subtract(start));
         }
 
     }
